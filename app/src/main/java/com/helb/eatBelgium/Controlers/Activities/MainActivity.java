001eb5128a1cfa9397afe6ee6177f90de4a32d57
Ayoub Bouziane <ayoub.bouziane4@gmail.com>
2020-05-13 19:54:45 +0200

    Notification with AlarmService

--- app/src/main/java/com/helb/eatBelgium/Controlers/Activities/MainActivity.java
+++ app/src/main/java/com/helb/eatBelgium/Controlers/Activities/MainActivity.java
@@ -13,7 +13,9 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.util.Calendar;
 
+import com.helb.eatBelgium.Notification.AlarmNotificationReceiver;
 import com.helb.eatBelgium.R;
 import com.helb.eatBelgium.Notification.NotificationPublisher;
 
@@ -29,8 +31,9 @@ public class MainActivity extends AppCompatActivity {
         btnSignUp = (Button)findViewById(R.id.btSignUp);
         btnSignIn = (Button)findViewById(R.id.btSignIn);
 
-        scheduleNotification(getNotification("30 second delay"), 10000);
+        //scheduleNotification(getNotification("30 second delay"), 10000);
 
+        startAlarm(true,true);
         //txtSlogan = (TextView)findViewById(R.id.txtSlogan);
        // Typeface face = Typeface.createFromAsset(getAssets(),"styleTexte.TTF");
        // txtSlogan.setTypeface(face);
@@ -77,5 +80,32 @@ public class MainActivity extends AppCompatActivity {
         builder.setContentText(content);
         builder.setSmallIcon(R.drawable.ic_contact_mail);
         return builder.build();
+    }
+
+    private void startAlarm(boolean isNotification, boolean isRepeat) {
+        AlarmManager manager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Intent myIntent;
+        PendingIntent pendingIntent;
+
+        // SET TIME HERE
+        //Calendar calendar= Calendar.getInstance();
+        //calendar.set(Calendar.HOUR_OF_DAY,5);
+        //calendar.set(Calendar.MINUTE,33);
+
+        Calendar now = Calendar.getInstance();
+
+        Calendar tmp = (Calendar) now.clone();
+        tmp.add(Calendar.MINUTE, 1);
+        Calendar nowPlus1Minute = tmp;
+
+
+        myIntent = new Intent(MainActivity.this, AlarmNotificationReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this,0,myIntent,0);
+
+
+        if(!isRepeat)
+            manager.set(AlarmManager.RTC_WAKEUP, SystemClock.elapsedRealtime()+3000,pendingIntent);
+        else
+            manager.setRepeating(AlarmManager.RTC_WAKEUP, nowPlus1Minute.getTimeInMillis(), AlarmManager.INTERVAL_DAY,pendingIntent);
     }
 }
---
+++ app/src/main/java/com/helb/eatBelgium/Notification/AlarmNotificationReceiver.java
@@ -0,0 +1,43 @@
+package com.helb.eatBelgium.Notification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import com.helb.eatBelgium.R;
+
+
+import androidx.core.app.NotificationCompat;
+
+import com.helb.eatBelgium.Controlers.Activities.MainActivity;
+
+public class AlarmNotificationReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        Intent myIntent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                myIntent,
+                PendingIntent.FLAG_ONE_SHOT );
+
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Zodiac")
+                .setContentIntent(pendingIntent)
+                .setContentText("Check out your horoscope")
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                .setContentInfo("Info");
+
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1,builder.build());
+    }
+}
