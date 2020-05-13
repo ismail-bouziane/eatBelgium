532587501dedc7d027ece2d30c07827ee3479023
Ayoub Bouziane <ayoub.bouziane4@gmail.com>
2020-05-13 22:37:23 +0200

    Notification receiver works !

--- app/src/main/java/com/helb/eatBelgium/Notification/AlarmNotificationReceiver.java
+++ app/src/main/java/com/helb/eatBelgium/Notification/AlarmNotificationReceiver.java
@@ -1,11 +1,14 @@
 package com.helb.eatBelgium.Notification;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
 import com.helb.eatBelgium.R;
 
 
@@ -38,6 +41,18 @@ public class AlarmNotificationReceiver extends BroadcastReceiver {
                 .setContentInfo("Info");
 
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // === Removed some obsoletes
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            String channelId = "Your_channel_id";
+            NotificationChannel channel = new NotificationChannel(
+                    channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channelId);
+        }
         notificationManager.notify(1,builder.build());
     }
 }
